41cc468fb450b47425afb0027ef8eea8b163deb7
Nicholas Cathcart <nicholascathcart@Nicholass-MacBook-Air.local>
2023-01-26 16:44:46 -0500

    added component tests

--- testing-utils/mockFetchVideo.js
+++ testing-utils/mockFetchVideo.js
@@ -7,8 +7,7 @@ const __dirname = path.dirname(__filename);
 
 export const mockFetchVideo = async () => {
   let video = await fs.promises.readFile(path.resolve(__dirname, '../uploads/c83e23fc71e00de195b688dbb56a0849'))
-  console.log('video is: ', video)
-  return await video;
+  return video;
 }
 
 
